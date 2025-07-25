f8e69f812e0035bc7a300abec87573a3f6eb0ceb
astridayala <astriayala06@gmail.com>
2025-07-25 15:33:08 -0600

    agregando tracking

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const SITE_ID = '98dba1e9-a518-46ac-8eaf-49df416a3b28';
+
+function WrapperApp() {
+  useEffect(() => {
+    if (window.PixelFactoryAnalytics) {
+      window.PixelFactoryAnalytics.trackPageView(SITE_ID);
+      window.PixelFactoryAnalytics.setupClickTracking(SITE_ID);
+    }
+  }, []);
+
+  return <App />;
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <WrapperApp />
   </StrictMode>,
 )
